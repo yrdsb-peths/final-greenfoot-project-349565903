0cfbbf30e3df643c3217835d62bc56e5dfc508e8
349565903 <118018441+349565903@users.noreply.github.com>
2023-01-17 14:35:17 -0500

    past present, futrue.

--- cosmos.java
+++ cosmos.java
@@ -2,7 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class cosmos here.
- * 
+ * past, present, futrue 
  * @author (your name) 
  * @version (a version number or a date)
  */
